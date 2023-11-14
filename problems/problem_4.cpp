980c7ed3134e0febd9558bd47140346397a72716
Nozimjon Olimjonov <muminovakmaljon713@gmail.com>
2023-11-14 23:17:25 +0500

    solved

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,20 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    int number = 0;
+    std::string answer;
+    if (macAddress[0] == 'F' and macAddress[1] == 'F' ){
+        answer =  "Broadcast";
+    }else {
+        number = (macAddress[1] - '0');
+        if(number > 9) number ++;
+        if (number % 2 == 0) {
+            answer = "Unicast";
+        } else {
+            answer = "Multicast";
+        }
+    }
+    return answer;
     // make use of control flow statements
     // return result;
 }
