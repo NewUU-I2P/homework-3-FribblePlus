de5a7ccdfff439545183c7c72247d37349a538ba
Nozimjon Olimjonov <muminovakmaljon713@gmail.com>
2023-11-14 23:36:16 +0500

    5 is changed

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,13 +1,13 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-   if(char == '+'){
+   if(operation == '+'){
        result = x + y;
-   }else if(char == '-'){
+   }else if(operation == '-'){
        result = x - y;
-   }else if(char == '*'){
+   }else if(operation == '*'){
        result = x * y;
-   }else if(char == '/'){
+   }else if(operation == '/'){
        result = x / y;
    }
    return result;
